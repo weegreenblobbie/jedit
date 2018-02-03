aab4cd7c096b06eca23e8ec4b74a5af2edb47e5f
Nick Hilton <weegreenblobbie2@gmail.com>
2018-02-03 14:46:02 -0800

    first commit, building jEdit working

---
+++ README.md
@@ -0,0 +1,22 @@
+How to build jEdit 5.3.0 on Windows 10 64-bit
+---------------------------------------------
+
+# Install the git client for windows, for basic POXIX commands like `unzip`, `tar`, and `patch`
+# Install Python for Windodws to be able to run `build.py`.
+# Install the Java JDK 64-bit from Oracle, Java SE Development Kit 8u162 worked for me.
+# Download the [jEdit source code](https://sourceforge.net/projects/jedit/files/jedit/5.3.0/jedit5.3.0source.tar.bz2/download) to the local dir
+# Download the [Apache Ant](http://mirror.olnevhost.net/pub/apache//ant/binaries/apache-ant-1.10.1-bin.zip) to the local dir
+# Inspect `config.json` to ensure paths and names are correct.
+
+Then just run the build script:
+
+    python build.py
+    # ...
+    BUILD SUCCESSFUL
+    Total time: 1 minute 35 seconds
+
+You may need to exit an existing jEdit session before running the one you built:
+
+    cd jEdit\build && jedit.jar
+
+Click on Help -> About jEdit to check the version.
---
+++ build.py
@@ -0,0 +1,118 @@
+import argparse
+import glob
+import json
+import os.path
+import subprocess
+import sys
+
+
+def run_cmd(*args, **kwargs):
+
+    print(*args)
+    sys.stdout.flush()
+
+    subprocess.check_call(*args, **kwargs, shell=True)
+
+
+def extract_ant_zip(config):
+    '''
+    extracts apache ant zip file
+    '''
+
+    #-------------------------------------------------------------------------
+    # was it already extracted?
+
+    if any([os.path.isdir(x) for x in glob.glob("apache-ant*")]):
+        print("apache-ant already extracted")
+
+    else:
+        cmd = '"{unzip_exe}" -q "{ant_tarball}"'.format(**config)
+        run_cmd(cmd)
+
+    ant_dir = None
+
+    for d in glob.glob("apache-ant*"):
+
+        if os.path.isdir(d):
+            ant_dir = d
+            break
+
+    if not ant_dir:
+        raise AssertionError("Failed to extract apache ant zip!")
+
+    config['ant_exe'] = os.path.join(ant_dir, 'bin', 'ant')
+
+
+def extract_jedit_tarball(config):
+    '''
+    extracts jedit tarball
+    '''
+
+    #-------------------------------------------------------------------------
+    # was it already extracted?
+
+    if any([os.path.isdir(x) for x in glob.glob("jEdit")]):
+        print("jEdit already extracted")
+        return
+
+    #-------------------------------------------------------------------------
+    # unzip
+
+    cmd = '"{tar_exe}" xfj "{jedit_tarball}"'.format(**config)
+
+    run_cmd(cmd)
+
+    assert os.path.isdir('jEdit'), "Failed to extract jEdit tarball!"
+
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--config',
+        type = str,
+        default = 'config.json',
+        help = 'The input config.json to process'
+    )
+
+    args = parser.parse_args()
+
+    #-------------------------------------------------------------------------
+    # read config
+
+    with open(args.config, 'r') as fd:
+        config = json.load(fd)
+
+    #-------------------------------------------------------------------------
+    # unpack tools
+
+    extract_ant_zip(config)
+    extract_jedit_tarball(config)
+
+    #-------------------------------------------------------------------------
+    # patch ivysettings.xml
+
+    cmd = 'cd jEdit && patch --forward < ..\ivysettings.xml.patch || echo ignoring exit code'
+
+    run_cmd(cmd)
+
+    #-------------------------------------------------------------------------
+    # build jEdit
+
+    # setup java_home
+
+    os.environ['JAVA_HOME'] = config['jdk_dir']
+
+    cmd = 'cd jEdit && "..\{ant_exe}"'.format(**config)
+
+    run_cmd(cmd)
+
+    assert os.path.isfile("jEdit/build/jedit.jar"), "Failed to build jedit.jar!"
+
+
+
+if __name__ == "__main__": main()
